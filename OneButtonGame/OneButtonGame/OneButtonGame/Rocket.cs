91a48cfc20b1c40931f210d9c2bb7d1591196391
Albin <albinn.sh@gmail.com>
2016-02-19 14:29:50 +0100

    I do not even knoooooow

--- OneButtonGame/OneButtonGame/OneButtonGame/Rocket.cs
+++ OneButtonGame/OneButtonGame/OneButtonGame/Rocket.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using 
 
 namespace OneButtonGame
 {
@@ -12,9 +14,11 @@ namespace OneButtonGame
             
         }
 
+        public override void Update(
+
         public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(AssetManager.SpriteSheet, AssetManager.
+            spriteBatch.Draw(AssetManager.SpriteSheet, AssetManager.SpriteSheetSourceRect(new Point(0, 0), new Point(256, 256)), Color.White);
         }
 
     }
